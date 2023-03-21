eae1a0dd1ed83d2a8fd7e46589f73fb3861cbb52
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-03-21 20:42:39 +0200

    fix

--- js/modal.js
+++ js/modal.js
@@ -8,7 +8,8 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
+	function toggleModal() {
+	document.body.classList.toggle('modal-open')
     refs.modal.classList.toggle('is-hidden');
   }
 })();
